085598abbd8cd8c55e85652734504b4ed1da34ed
Ravi Kumar Vishwakarma <47824408+Ravik27280@users.noreply.github.com>
2021-10-03 09:20:58 +0530

    complete

---
+++ Roman_To_Interger.py
@@ -0,0 +1,19 @@
+dic={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+n=input()
+res=0
+pre=0
+for i in range(len(n)):
+    s1=dic[n[i]]
+    if (i+1<len(n)):
+        s2=dic[n[i+1]]
+        if s1>=s2:
+            res+=s1
+        else:
+            res-=s1
+    else:
+        res+=s1
+
+  
+
+print(res)
+    
