3e8ba092205c39d6d05aa06c74e0b0a6b3266636
Ravi Kumar Vishwakarma <47824408+Ravik27280@users.noreply.github.com>
2021-10-04 14:55:56 +0530

    update

---
+++ Island Perimeter.py
@@ -0,0 +1,15 @@
+class Solution:
+    def islandPerimeter(self, grid) -> int:
+        m, n= len(grid),len(grid[0])
+        res=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]:
+                    res+=4
+                    if i>0 and grid[i-1][j]:
+                        res-=2
+                    if j>0 and grid[i][j-1]:
+                        res-=2
+        return res
+                
+        
