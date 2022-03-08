04f7a4a3876201186ab368fd07d3b72c4c6265c0
Ravi Kumar Vishwakarma <47824408+Ravik27280@users.noreply.github.com>
2022-03-09 02:23:25 +0530

    new update

---
+++ java/MergeTwoSortedLists.java
@@ -0,0 +1,26 @@
+ class ListNode1{
+    int val;
+     ListNode1 next;
+     ListNode1() {}
+     ListNode1(int val) { this.val = val; }
+     ListNode1(int val, ListNode1 next) { this.val = val; this.next = next; }
+  }
+
+class Solution {
+    public ListNode1 mergeTwoLists(ListNode1 list1, ListNode1 list2) {
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        if(list1.val<list2.val){
+            list1.next=mergeTwoLists(list1.next,list2);
+            return list1;
+        }
+        else{
+            list2.next=mergeTwoLists(list2.next,list1);
+        }
+        return list2;
+
+    }
+}
+
+public class MergeTwoSortedLists {
+}
