31cfd55a88428d7d0304076860605be1de703254
Ravi Kumar Vishwakarma <47824408+Ravik27280@users.noreply.github.com>
2022-03-09 01:59:14 +0530

    update java code

---
+++ java/Linkedlist_cycle.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+ }
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null) return false;
+        Set<ListNode> set=new HashSet<>();
+        while(head!=null){
+            if(set.contains(head)){
+                return true;
+            }
+            set.add(head);
+            head=head.next;
+
+        }
+        return false;
+    }
+}
+
+public class Linkedlist_cycle {
+}}
